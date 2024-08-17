d64875e4672956b8a50670cf6f3df9dfd81bf60b
Stephen Hodgson <rage.against.the.pixel@gmail.com>
2024-08-16 21:15:12 -0400

    upm-subtree-split@v1.1.3 (#6)
    - fix path matching

--- src/index.ts
+++ src/index.ts
@@ -9,13 +9,13 @@ const main = async () => {
     try {
         const packageRootInput = core.getInput(`package-root`) || `**/Packages/com.*`;
         let packagePath = undefined;
-        const globber = await glob.create(packageRootInput, { followSymbolicLinks: false });
+        const globber = await glob.create(packageRootInput, { followSymbolicLinks: false, implicitDescendants: false });
         const files = await globber.glob();
         if (files.length === 0) {
             throw Error('No package found.');
         }
         if (files.length > 1) {
-            throw Error('Multiple packages found!');
+            throw Error(`Multiple packages found!\n${files.join('\n')}`);
         }
         packagePath = files[0].replace(`${WORKSPACE}/`, '');
         await exec.exec('git', ['subtree', 'split', '--prefix', packagePath, '-b', 'upm']);
